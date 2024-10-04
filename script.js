3e6e754f6ab3bd11476ea892c836e6e640541feb
Emyeeeel <maryamielcatado@gmail.com>
2024-10-04 16:57:36 +0800

    added html and script

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const first = document.getElementById('first-num');
+const second = document.getElementById('second-num');
+const result = document.getElementById('result');
+
+function multiply(){
+    const num1 = parseInt(first.value, 10);
+    const num2 = parseInt(second.value, 10);
+    result.innerHTML = 'Result: ' + (num1 * num2);
+}
+
+function divide(){
+    const num1 = parseInt(first.value, 10);
+    const num2 = parseInt(second.value, 10);
+    if (num2 !== 0) {
+        result.innerHTML = 'Result: ' + (num1 / num2);
+    } else {
+        result.innerHTML = 'Error: Division by zero is not allowed.';
+    }
+}
